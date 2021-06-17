909b0c096cb79b1957104dd24d7bee23f1471528
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-17 15:08:36 -0300

    💄 New blade components layout

---
+++ resources/CareUS/layouts/parts/header.blade.php
@@ -0,0 +1,39 @@
+<header class="flex flex-col w-full border-b">
+    <div class="flex flex-col items-center justify-between w-full md:flex-row">
+        <div class="flex items-center justify-center w-full md:w-1/4">
+            {{ config('app.name') }}
+        </div>
+        <nav class="flex items-center justify-center w-full md:w-3/4">
+            <ul class="flex flex-col items-center justify-around w-full md:flex-row">
+                <li>HEADER ITEM</li>
+                <li>HEADER ITEM</li>
+            </ul>
+            <ul class="flex flex-col items-center justify-around w-full md:flex-row">
+                <li>HEADER ITEM</li>
+                <li>HEADER ITEM</li>
+            </ul>
+            <ul class="flex flex-col items-center justify-around w-full md:flex-row">
+                <li>HEADER ITEM</li>
+                <li>HEADER ITEM</li>
+            </ul>
+        </nav>
+    </div>
+    <nav class="flex flex-row items-center justify-center w-full border-t">
+        <ul class="flex flex-col items-center justify-around w-1/2 md:flex-row">
+            <li>SUBHEADER ITEM</li>
+            <li>SUBHEADER ITEM</li>
+        </ul>
+        <ul class="flex flex-col items-center justify-around w-1/2 md:flex-row">
+            <li>SUBHEADER ITEM</li>
+            <li>SUBHEADER ITEM</li>
+        </ul>
+        <ul class="flex flex-col items-center justify-around w-1/2 md:flex-row">
+            <li>SUBHEADER ITEM</li>
+            <li>SUBHEADER ITEM</li>
+        </ul>
+        <ul class="flex flex-col items-center justify-around w-1/2 md:flex-row">
+            <li>SUBHEADER ITEM</li>
+            <li>SUBHEADER ITEM</li>
+        </ul>
+    </nav>
+</header>
---
+++ resources/CareUS/layouts/parts/sidebar.blade.php
@@ -0,0 +1,25 @@
+<button @click.prevent="open = (open === 'false') ? 'true' : 'false'"
+    :class="(open === 'false') ? '-ml-6 left-0' : '-ml-6 left-0'"
+    class="z-10 flex-col items-center justify-center hidden w-10 h-10 rounded-full md:sticky md:flex focus:outline-none focus:ring-0 top-16 bg-secondary-900 text-light-500">
+    <i :class="(open === 'false') ? 'fa-angle-left mr-1' : 'fa-angle-right'" class="text-2xl fa "></i>
+</button>
+
+<div :class="(open === 'false') ? 'hidden' : ''"
+    class="z-0 flex flex-col items-start justify-between w-full min-h-full md:absolute">
+    <div class="flex flex-row flex-grow w-full md:flex-col">
+
+        <header class="flex items-center justify-center w-1/3 border-t md:border-t-0 border-secondary-800 md:w-full">
+            SIDEBAR HEADER
+        </header>
+
+        <main class="flex items-center justify-center w-2/3 border-t border-secondary-800 md:flex-grow md:w-full">
+            SIDEBAR CONTENT
+        </main>
+
+    </div>
+
+    <footer class="items-center justify-center hidden w-full border-t border-secondary-800 md:flex">
+        SIDEBAR FOOTER
+    </footer>
+
+</div>
