c2978222f1d53bf04eeaabce91e5963e27de5b5a
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-22 01:56:24 -0300

    🚧 Show user data

--- resources/CareUS/pages/users/profile.blade.php
+++ resources/CareUS/pages/users/profile.blade.php
@@ -3,7 +3,7 @@
     <x-slot name="subheader">
         <h2
             class="flex flex-col items-start justify-center order-2 w-full px-6 mt-3 text-xl font-semibold leading-loose border-t md:px-0 md:mt-0 md:w-1/2 md:ml-12 md:text-2xl md:flex-row md:order-1 border-dark-50 md:border-t-0 md:items-center md:justify-start">
-            {{ __(":name profile", ['name' => $user->name]) }}
+            {{ __(":name profile", ['name' => auth()->user()->persona->formated_name]) }}
         </h2>
         <div
             class="flex flex-col items-start justify-center order-1 w-full md:px-6 md:pr-12 md:w-1/2 md:justify-center md:items-center md:flex-row md:order-2">
@@ -14,6 +14,11 @@
 
     <div class="w-full p-6 md:px-12 text-cemter">
         {{ __("Profile") }}
+
+        <pre class="break-words whitespace-normal">
+        {{ auth()->user() }}
+        {{ auth()->user()->persona }}
+        </pre>
     </div>
 
 </x-careus-layout>
