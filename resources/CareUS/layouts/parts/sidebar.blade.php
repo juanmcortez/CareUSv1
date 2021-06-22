f022a8c241d10f320eefd4cd2601968dc3bcbbf9
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-22 01:56:06 -0300

    🚧 Show last login time

--- resources/CareUS/layouts/parts/sidebar.blade.php
+++ resources/CareUS/layouts/parts/sidebar.blade.php
@@ -22,8 +22,8 @@
 
     <footer
         class="flex flex-col items-center justify-end w-1/3 px-6 py-6 border-t md:flex-row md:py-9 md:px-0 md:justify-center md:w-full text-dark-400 border-secondary-800 md:sticky md:bottom-0 md:left-0">
-        <p class="md:mr-5">
-            {{ __("Last login: :date", ['date' => date('M d, Y', strtotime(auth()->user()->updated_at))]) }}
+        <p class="text-xs md:mr-5">
+            {{ __("Last login: :date", ['date' => date('M d, Y H:i', strtotime(auth()->user()->last_login_at))]) }}
         </p>
         <form method="POST" action="{{ route('logout') }}" class="mt-3 md:mt-0">
             @csrf
