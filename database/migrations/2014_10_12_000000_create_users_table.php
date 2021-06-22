9b8795e8a1f511f75239daef42f9fb85c6ba44a3
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-22 02:22:16 -0300

    🗃 Make last login at nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
 
             $table->timestamp('email_verified_at')->nullable();
 
-            $table->dateTime('last_login_at');
+            $table->dateTime('last_login_at')->nullable();
             $table->rememberToken();
 
             $table->softDeletes();
