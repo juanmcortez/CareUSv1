9f9d368905f12808dbaf86ea07e57ec39f93101b
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-23 17:42:42 -0300

    💄 New notifycations style

--- resources/CareUS/components/alerts/ribbon.blade.php
+++ resources/CareUS/components/alerts/ribbon.blade.php
@@ -4,23 +4,21 @@
 'title' => 'Info',
 'description'
 ])
-<div class="w-full border text-{{ $color }}-600 bg-{{ $color }}-100 border-{{ $color }}-200">
-    <div class="flex items-center justify-between px-10 py-3">
-        <div class="flex flex-col">
+<div
+    class="absolute flex flex-row w-full max-w-xl mx-auto overflow-hidden bg-{{ $color }}-50 rounded-lg shadow-md top-32 mt-1 right-32 dark:bg-gray-800">
+    <div class="flex flex-row flex-shrink-0 items-center justify-center w-16 bg-{{ $color }}-500">
+        <i class="text-xl fa fa-{{ $icon }} text-{{ $color }}-50"></i>
+    </div>
+    <div class="flex flex-row items-center justify-start w-full px-4 py-2 -mx-3">
+        <div class="w-full mx-3">
+            <span class="font-semibold text-{{ $color }}-700 uppercase">{{ __($title) }}</span>
             @if (is_array($description))
             @foreach ($description as $itdx => $item)
-            <p class="@if($itdx > 0) mt-1 @endif text-sm tracking-wide">
-                <strong class="mr-1 uppercase">{{ __($title) }}</strong>
-                <i class="mr-1 text-xs fa fa-arrow-right"></i>
-                {!! $item !!}
-            </p>
+            <p class="mt-1 text-sm text-{{ $color }}-700">{!! $item !!}</p>
             @endforeach
             @else
-            <p class="text-sm tracking-wide">
-                <strong class="mr-2 uppercase">{{ __($title) }}</strong> {!! $description !!}
-            </p>
+            <p class="mt-1 text-sm text-{{ $color }}-700">{!! $description !!}</p>
             @endif
         </div>
-        <i class="text-lg fa fa-{{ $icon }}"></i>
     </div>
 </div>
