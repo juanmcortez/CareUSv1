0bc20f159a88c5b41e6827ebc28cbf3ca6fa64ef
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-26 20:57:55 -0300

    🚧 New Eligibilities CMFM

---
+++ app/Models/Eligibilities/Eligibility.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Eligibilities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Eligibility extends Model
+{
+    use HasFactory;
+}
