b32e773903c074edd33bfb7336faa81569edb2a8
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-26 20:56:07 -0300

    🚧 New Billings CMFM

---
+++ app/Http/Controllers/Billings/BillingController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Billings;
+
+use App\Http\Controllers\Controller;
+use App\Models\Billings\Billing;
+use Illuminate\Http\Request;
+
+class BillingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Billings\Billing  $billing
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Billing $billing)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Billings\Billing  $billing
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Billing $billing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Billings\Billing  $billing
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Billing $billing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Billings\Billing  $billing
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Billing $billing)
+    {
+        //
+    }
+}
