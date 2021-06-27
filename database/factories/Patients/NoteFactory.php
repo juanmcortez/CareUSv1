f7e69003d1d54ff02cca2c942dba50cf4fc349a7
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-26 21:13:41 -0300

    🚧 New Patient Notes CMFM

---
+++ database/factories/Patients/NoteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Patients;
+
+use App\Models\Patients\Note;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NoteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Note::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
