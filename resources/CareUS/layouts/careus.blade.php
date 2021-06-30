387e207b9796e6e2a55ece609b9e36b7c0846ce1
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-06-30 15:33:21 -0300

    🚧 Show/Hide password on login

--- resources/CareUS/layouts/careus.blade.php
+++ resources/CareUS/layouts/careus.blade.php
@@ -34,6 +34,11 @@
         {{ $slot }}
     </main>
 
+    <!-- Scripts -->
+    <script src=" {{ mix('/js/manifest.js') }}"></script>
+    <script src="{{ mix('/js/vendor.js') }}"></script>
+    <script src="{{ mix('/js/careus.js') }}" defer></script>
+
 </body>
 
 @endguest
